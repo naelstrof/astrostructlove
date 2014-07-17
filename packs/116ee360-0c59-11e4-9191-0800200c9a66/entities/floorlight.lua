b35bf220b0fb9bf32bb83551f6189640db9b7ed5
Ericxgao <Ericgao@berkeley.edu>
2014-07-16 17:02:17 -0700

    Added a lighted floor tile

---
+++ packs/116ee360-0c59-11e4-9191-0800200c9a66/entities/floorlight.lua
@@ -0,0 +1,16 @@
+floorlight = {
+    __name = "floorlight",
+    components = {
+        Components.drawable,
+        Components.floor,
+        Components.emitslight,
+        Components.networked,
+        Components.default
+    },
+    image = PackLocation .. "textures/floorlight.png",
+    attributes = {
+        drawable = love.graphics.newImage( PackLocation .. "textures/floorlight.png" )
+    }
+}
+
+return floorlight

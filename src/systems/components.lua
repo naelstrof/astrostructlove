f3db92d384bd51e46cc3bd8db65d130704ae4a3b
naelstrof <naelstrof@gmail.com>
2014-07-21 15:16:25 -0600

    Fixed a pack loading bug

--- src/systems/components.lua
+++ src/systems/components.lua
@@ -4,18 +4,22 @@ function Components:load( dir, packlocation )
     local files = love.filesystem.getDirectoryItems( dir )
     for i,v in pairs( files ) do
         local s = string.find( v, "%.lua" )
-        local str = string.sub( v, 0, s - 1 )
-        PackLocation = packlocation
-        local component = require( dir .. str )
-        if type( component ) == "boolean" then
-            error( "Failed to load component " .. dir .. v .. " nothing was returned!" )
-        elseif not component.__name then
-            error( "Failed to load component " .. dir .. v .. " __name not supplied" )
-        else
-            if self[ str ] ~= nil then
-                print( "Overriden component " .. component.__name .. " with " .. v )
+        if s then
+            local str = string.sub( v, 0, s - 1 )
+            PackLocation = packlocation
+            local component = require( dir .. str )
+            if type( component ) == "boolean" then
+                error( "Failed to load component " .. dir .. v .. " nothing was returned!" )
+            elseif not component.__name then
+                error( "Failed to load component " .. dir .. v .. " __name not supplied" )
+            else
+                if self[ str ] ~= nil then
+                    print( "Overriden component " .. component.__name .. " with " .. v )
+                end
+                self[ str ] = component
             end
-            self[ str ] = component
+        else
+            print( "Skipped " .. dir .. v .. " because it's not a lua file..." )
         end
     end
 end
--- src/systems/entities.lua
+++ src/systems/entities.lua
@@ -4,21 +4,25 @@ function Entities:load( dir, packlocation )
     local files = love.filesystem.getDirectoryItems( dir )
     for i,v in pairs( files ) do
         local s = string.find( v, "%.lua" )
-        local str = string.sub( v, 0, s - 1 )
-        PackLocation = packlocation
-        local ents = require( dir .. str )
-        if type( ents ) == "boolean" then
-            error( "Failed to load entities " .. dir .. v .. " nothing was returned!" )
-        end
-        for o,ent in pairs( ents ) do
-            if not ent.__name then
-                print( "Failed to load entity " .. dir .. v .. " __name not supplied" )
-            else
-                if self.entities[ ent.__name ] ~= nil then
-                    print( "Overriden entity " .. ent.__name .. " with " .. v )
-                end
-                self.entities[ ent.__name ] = ent
+        if s then
+            local str = string.sub( v, 0, s - 1 )
+            PackLocation = packlocation
+            local ents = require( dir .. str )
+            if type( ents ) == "boolean" then
+                error( "Failed to load entities " .. dir .. v .. " nothing was returned!" )
             end
+            for o,ent in pairs( ents ) do
+                if not ent.__name then
+                    print( "Failed to load entity " .. dir .. v .. " __name not supplied" )
+                else
+                    if self.entities[ ent.__name ] ~= nil then
+                        print( "Overriden entity " .. ent.__name .. " with " .. v )
+                    end
+                    self.entities[ ent.__name ] = ent
+                end
+            end
+        else
+            print( "Skipped " .. dir .. v .. " because it's not a lua file..." )
         end
     end
     self:generateNetworkInfo()
